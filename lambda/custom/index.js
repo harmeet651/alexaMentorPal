e23408a9d20015604ed0d73feaf9055856b9d0a7
harmeet651 <32997386+harmeet651@users.noreply.github.com>
2018-07-13 18:57:19 -0700

    First commit

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -6,7 +6,7 @@ var streamInfo = {
   title: 'Audio Stream Starter',
   subtitle: 'A starter template for an Alexa audio streaming skill.',
   cardContent: "Get more details at: https://skilltemplates.com",
-  url: 'https://streaming.radionomy.com/RadioXUS?lang=en-US&appName=iTunes.m3u',
+  url: 'https://s3.amazonaws.com/harmeetsingh/Daaru+Band-(Mr-Jatt.com).mp3',
   image: {
     largeImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-lg.png',
     smallImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-sm.png'
@@ -25,8 +25,57 @@ exports.handler = (event, context, callback) => {
 };
 
 var handlers = {
+
   'LaunchRequest': function() {
-    this.emit('PlayStream');
+    this.emit('MentorPalIntent');
+  },
+  'MentorPalIntent': function () {
+    var MentorPals = {
+
+    "questionone" : {
+        "ansNumber": "answer",
+        "answer": "is this"
+    }, 
+    "questiontwo" : {
+        "ansNumber": "answer",
+        "answer": "is this"
+    }, 
+    "questionthree" : {
+        "ansNumber": "Answer",
+        "answer": "is this"
+    }, 
+    "questionfour" : {
+        "ansNumber": "Answer",
+        "answer": "is this"
+    }, 
+    "questionfive" : {
+        "ansNumber": "Answer",
+        "answer": "is this"
+    }, 
+    "question six" : {
+        "ansNumber": "Answer",
+        "answer": "is this"
+    }, 
+    "questionseven" : {
+        "ansNumber": "Answer",
+        "answer": "is this"
+     }
+}
+     var speechOutput = "Welcome to Mentor Pal! I can tell you about our US navy heroes. I can only give answers about one question at a time. What question are you interested in, question one, two, three?"
+
+     var reprompt = "Which question are you interested in? You can say question one, question two and so on."
+
+    // var header = "Mentor Pal Q/A!"
+
+    // var shouldEndSession = false
+
+    // var sessionAttributes = {
+    //     "speechOutput" : speechOutput,
+    //     "repromptText" : reprompt
+    // }
+
+    // callback(sessionAttributes, buildSpeechletResponse(header, speechOutput, reprompt, shouldEndSession))
+    this.emit(':ask', speechOutput);
   },
   'PlayStream': function() {
     this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
