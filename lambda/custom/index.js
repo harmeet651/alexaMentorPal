45b47ef10819ed70b7ed1726e89dc4aad0710b7c
harmeet651 <32997386+harmeet651@users.noreply.github.com>
2018-07-17 18:20:54 -0700

    First commit

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -6,7 +6,7 @@ var streamInfo = {
   title: 'Audio Stream Starter',
   subtitle: 'A starter template for an Alexa audio streaming skill.',
   cardContent: "Get more details at: https://skilltemplates.com",
-  url: 'https://s3.amazonaws.com/harmeetsingh/Daaru+Band-(Mr-Jatt.com).mp3',
+  url: 'https://streaming.radionomy.com/RadioXUS?lang=en-US&appName=iTunes.m3u',
   image: {
     largeImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-lg.png',
     smallImageUrl: 'https://s3.amazonaws.com/cdn.dabblelab.com/img/alexa-card-sm.png'
@@ -27,56 +27,82 @@ exports.handler = (event, context, callback) => {
 var handlers = {
 
   'LaunchRequest': function() {
-    this.emit('MentorPalIntent');
+    //this.emit('tell','Tell a question number');
+    this.emit(':ask', 'Hello World!');
   },
-  'MentorPalIntent': function () {
-    var MentorPals = {
-
-    "questionone" : {
-        "ansNumber": "answer",
-        "answer": "is this"
-    }, 
-    "questiontwo" : {
-        "ansNumber": "answer",
-        "answer": "is this"
-    }, 
-    "questionthree" : {
-        "ansNumber": "Answer",
-        "answer": "is this"
-    }, 
-    "questionfour" : {
-        "ansNumber": "Answer",
-        "answer": "is this"
-    }, 
-    "questionfive" : {
-        "ansNumber": "Answer",
-        "answer": "is this"
-    }, 
-    "question six" : {
-        "ansNumber": "Answer",
-        "answer": "is this"
-    }, 
-    "questionseven" : {
-        "ansNumber": "Answer",
-        "answer": "is this"
-     }
-}
-     var speechOutput = "Welcome to Mentor Pal! I can tell you about our US navy heroes. I can only give answers about one question at a time. What question are you interested in, question one, two, three?"
-
-     var reprompt = "Which question are you interested in? You can say question one, question two and so on."
-
-    // var header = "Mentor Pal Q/A!"
-
-    // var shouldEndSession = false
-
-    // var sessionAttributes = {
-    //     "speechOutput" : speechOutput,
-    //     "repromptText" : reprompt
-    // }
-
-    // callback(sessionAttributes, buildSpeechletResponse(header, speechOutput, reprompt, shouldEndSession))
-    this.emit(':ask', speechOutput);
+  'IntentToCallQues' : function() {
+    var start = this.event.request.intent.slots["NUMBER"].value;
+      //this.emit(':ask', start);
+      if(!start)
+      {
+        //this.emit(':ask', 'hello aaaaaaaaaa');
+        this.emit('Unhandled');
+      }
+      else
+      {
+        this.emit('PlayStream');
+      }
   },
+  //'MentorPalIntent': function () {
+
+//     var MentorPals = {
+
+//     "questionone" : {
+//         "ansNumber": "answer",
+//         "answer": "is this"
+//     }, 
+//     "questiontwo" : {
+//         "ansNumber": "answer",
+//         "answer": "is this"
+//     }, 
+//     "questionthree" : {
+//         "ansNumber": "Answer",
+//         "answer": "is this"
+//     }, 
+//     "questionfour" : {
+//         "ansNumber": "Answer",
+//         "answer": "is this"
+//     }, 
+//     "questionfive" : {
+//         "ansNumber": "Answer",
+//         "answer": "is this"
+//     }, 
+//     "question six" : {
+//         "ansNumber": "Answer",
+//         "answer": "is this"
+//     }, 
+//     "questionseven" : {
+//         "ansNumber": "Answer",
+//         "answer": "is this"
+//      }
+// }
+    
+//     //var guessNum = parseInt(this.event.request.intent.slots.number.value);
+
+//      var speechOutput = "Welcome to Mentor Pal! I can tell you about our US navy heroes. I can only give answers about one question at a time. What question are you interested in, question one, two, three?"
+
+//      //var reprompt = "Which question are you interested in? You can say question one, question two and so on."
+
+//     this.emit(':ask', speechOutput);
+
+//     const intentObj = this.event.request.intent;
+//     if (!intentObj.slots.MentorPal.value){
+//       console.log("ni aya");
+//         var speechOutput2 = "I do not have answer to this question. Try asking question one, question two, till seven";
+//         var repromptText = "Try asking any other question";
+//         //var header = "Not Valid question";
+//     } 
+//     else {
+//       console.log("aya");
+//         var ansNumber = MentorPals[MentorPal].ansNumber;
+//         var answer = MentorPals[MentorPal].answer;
+//         //var speechOutput2 = capitalizeFirst(MentorPal) + " " + ansNumber + " " + answer + ". Do you want to know more?";
+//         var speechOutput2="ayegegeggege";
+//         var repromptText = "Do you want to know anything else?";
+//         //var header = capitalizeFirst(MentorPal);
+//     }
+//     this.emit(':tell', PlayStream);
+ // },
   'PlayStream': function() {
     this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
     this.emit(':responseReady');
@@ -94,6 +120,8 @@ var handlers = {
     this.callback(null, null)
   },
   'Unhandled': function() {
+    this.emit(':tell', 'aa gya unhndled me');
+    this.emit(':tell', 'hahaha');
     this.response.speak('Sorry. Something went wrong.');
     this.emit(':responseReady');
   },
@@ -136,7 +164,7 @@ var handlers = {
   },
   'PlayCommandIssued': function() {
 
-    if (this.event.request.type === 'IntentRequest' || this.event.request.type === 'LaunchRequest') {
+    if (this.event.request.type === 'IntentRequest') {
       var cardTitle = streamInfo.subtitle;
       var cardContent = streamInfo.cardContent;
       var cardImage = streamInfo.image;
